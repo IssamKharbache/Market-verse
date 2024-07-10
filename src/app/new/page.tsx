8078a6fd81901838371371ff60afb60f16f21438
lefline <issamkharbache2@gmail.com>
2024-07-10 10:33:09 +0100

    adding google maps and location functionality

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -7,30 +7,46 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { UploadResponse } from "imagekit/dist/libs/interfaces";
 import { useState } from "react";
 
-const newListPage = () => {  
+const newListPage = () => {
+
+  const defaultLocation = {
+    lat:33.573616824951735,
+    lng:-7.617842797047212
+
+}
+
+//states
   const [files, setFiles] = useState<UploadResponse[]>([]);
-  const [location,setLocation] = useState<Location>()
+  const [location,setLocation] = useState<Location>(defaultLocation);
+  const [geoLocation,setGeoLocation] = useState<Location|null>(null);
+
+ //get current location function
+  const handleLocateMyPosition = () =>{
+    navigator.geolocation.getCurrentPosition((ev)=>{
+      const location = {lat: ev.coords.latitude, lng:ev.coords.longitude}
+       setLocation(location);
+       setGeoLocation(location);
+    }, console.error);
+  }
   return (
     <form className="max-w-[1000px] mx-auto flex gap-8 mt-8">
       {/* input fiels */}
       <div className="flex flex-col">
         <AddListingForm />
-      
-       
       </div>
-      {/* image uploader */}
+      {/* image uploader and location */}
       <div className="flex flex-col">
       <UploadArea files={files} setFiles={setFiles} />
       <div className="mt-6">
        <div className="flex items-center justify-between gap-6 mb-4">
-       <label htmlFor="where">Where is your listing located</label>
-       <button className="flex items-center border p-2 rounded bg-slate-100 hover:bg-slate-200 duration-200">
+       <label className="mb-2" htmlFor="where">Where is your listing located</label>
+       <button type="button" onClick={handleLocateMyPosition} className="flex items-center border p-2 rounded bg-slate-100 hover:bg-slate-200 duration-200">
         <FontAwesomeIcon icon={faLocationCrosshairs} />
         </button>
        </div>
           <div className="bg-slate-200 rounded p-4 min-h-12 text-gray-100">
           {/* map here */}
-          <LocationPicker  onChange={location => setLocation(location)}/>
+          <LocationPicker geoCords={geoLocation}  defaultLocation={location} onChange={(location)=>setLocation(location)}/>
           </div>
         </div>
       </div>
--- src/components/map/LocationPicker.tsx
+++ src/components/map/LocationPicker.tsx
@@ -1,6 +1,6 @@
 
 "use effect";
-import React, { createRef, useEffect } from 'react'
+import { createRef, useEffect} from 'react'
 import { Loader } from "@googlemaps/js-api-loader"
 
 export type Location ={
@@ -8,38 +8,37 @@ export type Location ={
     lng:number
 }
 
-type LocationHandler = (pos:Location) => void
-
-const LocationPicker = ({onChange}:{onChange:LocationHandler}) => {
+const LocationPicker = ({defaultLocation, onChange,geoCords}:{defaultLocation:Location,onChange:(location:Location)=>void ,geoCords:Location| null}) => {
     const mapRef = createRef<HTMLDivElement>();
-    const loadMap = async () =>{
+    const loadMap = async () => {
         const loader = new Loader({
             apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-          
           });
         const {Map} = await  loader.importLibrary("maps");
         const {AdvancedMarkerElement} = await loader.importLibrary("marker");
         const map = new Map(mapRef.current as HTMLDivElement,{
             mapId:"map",
-            center:{lat:0,lng:0},
+            center:defaultLocation,
             zoom:8,
             mapTypeControl:false,
-            streetViewControl:false
+            streetViewControl:false 
         });
-        
+
         const marker = new AdvancedMarkerElement({
-            map,position:{lat:0,lng:0},
+            map,position:defaultLocation,
         })
         map.addListener("click",(ev:any)=>{
             marker.position = ev?.latLng;
             const lat = ev.latLng.lat();
             const lng = ev.latLng.lng();
-            onChange({lat,lng})
+             onChange({lat,lng})
         })
     }
+   //
     useEffect(()=>{
         loadMap();
-    },[])
+    },[geoCords])
+
   return (
     <div ref={mapRef} id='mapElement' className='w-full h-[300px]' ></div>
   )
